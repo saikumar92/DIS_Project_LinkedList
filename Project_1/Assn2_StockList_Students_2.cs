2d98526d9efa61db1504b024d7606683084dcb16
saikumar92 <saikumar92@users.noreply.github.com>
2018-09-30 19:32:03 -0400

    Add project files.

---
+++ Project_1/Assn2_StockList_Students_2.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_2
+{
+  public partial class StockList
+  {
+    //param   (StockList)listToMerge : second list to be merged 
+    //summary      : merge two different list into a single result list
+    //return       : merged list
+    //return type  : StockList
+    public StockList MergeList(StockList listToMerge)
+    {
+      StockList resultList = new StockList();
+
+            // write your implementation here
+
+            var list1 = new List<StockNode>();
+
+            if (!this.IsEmpty())
+            {
+                var stn = new StockNode();
+                stn = this.head;
+                list1.Add(stn);
+
+                do
+                {
+                    stn = stn.Next;
+                    list1.Add(stn);
+                }
+                while (stn.Next != null);
+            }
+
+            var list2 = new List<StockNode>();
+
+            if (!listToMerge.IsEmpty())
+            {
+                var stn = new StockNode();
+                stn = listToMerge.head;
+                list2.Add(stn);
+
+                do
+                {
+                    stn = stn.Next;
+                    list2.Add(stn);
+                }
+                while (stn.Next != null);
+            }
+
+            for (int i = 0; i < list1.Count; i++)
+            {
+                var symbol = list1[i].StockHolding.Symbol;
+                var name = list1[i].StockHolding.Name;
+                var holdings = list1[i].StockHolding.Holdings;
+                var currentPrice = list1[i].StockHolding.CurrentPrice;
+                Stock stock = new Stock(symbol, name, holdings, currentPrice);
+
+                resultList.AddLast(stock);
+            }
+
+            for (int i = 0; i < list2.Count; i++)
+            {
+                var symbol = list2[i].StockHolding.Symbol;
+                var name = list2[i].StockHolding.Name;
+                var holdings = list2[i].StockHolding.Holdings;
+                var currentPrice = list2[i].StockHolding.CurrentPrice;
+                Stock stock = new Stock(symbol, name, holdings, currentPrice);
+
+                resultList.AddLast(stock);
+            }
+
+            return resultList;
+    }
+
+    //param        : NA
+    //summary      : finds the stock with most number of holdings
+    //return       : stock with most shares
+    //return type  : Stock
+    public Stock MostShares()
+    {
+      Stock mostShareStock = null;
+
+            // write your implementation here
+            if (!this.IsEmpty())
+            {
+                var stn = new StockNode();
+                stn = this.head;
+                decimal currentshareCount = stn.StockHolding.Holdings;
+                mostShareStock = stn.StockHolding;
+
+                do
+                {
+                    if(currentshareCount < stn.StockHolding.Holdings)
+                    {
+                        currentshareCount = stn.StockHolding.Holdings;
+                        mostShareStock = stn.StockHolding;
+                    }
+                    stn = stn.Next;
+                }
+                while (stn.Next != null);
+                
+            }
+
+
+            return mostShareStock;
+    }
+
+    //param        : NA
+    //summary      : finds the number of nodes present in the list
+    //return       : length of list
+    //return type  : int
+    public int Length()
+    {
+      int length = 0;
+
+            // write your implementation here
+
+            if (!this.IsEmpty())
+            {
+                var stn = new StockNode();
+                stn = this.head;
+                length++;
+
+                do
+                {
+                    length++;
+                    stn = stn.Next;
+                }
+                while (stn.Next != null);
+
+                return length;
+            }
+
+            return length;
+    }
+  }
+}
---
+++ Project_1/Assn2_StockList_Students_3.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment_2
+{
+  public partial class StockList
+  {
+    //param        : NA
+    //summary      : Calculate the value of each node by multiplying holdings with price, and returns the total value
+    //return       : total value
+    //return type  : decimal
+    public decimal Value()
+    {
+      decimal value = 0.0m;
+
+            // write your implementation here
+            if (!this.IsEmpty())
+            {
+                var stn = new StockNode();
+                stn = this.head;
+                value = value + stn.StockHolding.Holdings * stn.StockHolding.CurrentPrice;
+
+                do
+                {
+                    stn = stn.Next;
+                    value = value + stn.StockHolding.Holdings * stn.StockHolding.CurrentPrice;
+                }
+                while (stn.Next != null);
+            }
+
+            return value;
+    }
+
+    //param  (StockList) listToCompare     : StockList which has to comared for similarity index
+    //summary      : finds the similar number of nodes between two lists
+    //return       : similarty index
+    //return type  : int
+    public int Similarity(StockList listToCompare)
+    {
+      int similarityIndex = 0;
+
+      // write your implementation here
+      var list1 = new List<string>();
+
+            if (!this.IsEmpty())
+            {
+                var stn = new StockNode();
+                stn = this.head;
+                list1.Add(stn.StockHolding.Symbol);
+
+                do
+                {
+                    stn = stn.Next;
+                    list1.Add(stn.StockHolding.Symbol);
+                }
+                while (stn.Next != null);
+            }
+
+            var list2 = new List<string>();
+
+            if (!listToCompare.IsEmpty())
+            {
+                var stn = new StockNode();
+                stn = listToCompare.head;
+                list2.Add(stn.StockHolding.Symbol);
+
+                do
+                {
+                    stn = stn.Next;
+                    list2.Add(stn.StockHolding.Symbol);
+                }
+                while (stn.Next != null);
+            }
+
+            var CommonList = list1.Intersect(list2);
+            similarityIndex = CommonList.Count();
+
+      return similarityIndex;
+    }
+
+    //param        : NA
+    //summary      : Print all the nodes present in the list
+    //return       : NA
+    //return type  : NA
+    public void Print()
+    {
+            // write your implementation here
+
+            if (!this.IsEmpty())
+            {
+                var stn = new StockNode();
+                stn = this.head;
+                Console.WriteLine(stn.StockHolding.ToString());
+                
+                do
+                {
+                    stn = stn.Next;
+                    Console.WriteLine(stn.StockHolding.ToString());
+                }
+                while (stn.Next != null);
+                
+            }
+
+        }
+  }
+}
